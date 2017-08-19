74bce8d0cff7da03c9d0e12338ba160a5a07c96b
clturner <clturner23@gmail.com>
2017-08-19 20:49:01 +0000

    add sts method returns all clases and count

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,8 +4,26 @@ create routes for /status
 """
 from flask import Blueprint, render_template, make_response, jsonify
 from api.v1.views import app_views
+from models import city, place, review, state, user
+from models import storage, base_model, amenity
 
 
 @app_views.route('/status')
 def index():
     return make_response(jsonify({'status': 'OK'}))
+
+
+@app_views.route('/stats')
+def stats():
+    CNC = {
+        'Amenity': "amenities",
+        'City': "cities",
+        'Place': "places",
+        'Review': "reviews",
+        'State': "states",
+        'User': "users"
+    }
+    new_dict = {}
+    for key, value in CNC.items():
+        new_dict[value] = storage.count(key)
+    return jsonify(new_dict)
