72254f4f2f8d3528705765d92155b29f42aa63a1
Christian Agha <christianagha@gmail.com>
2017-09-14 22:29:23 -0700

    adding problem #13

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,10 +4,12 @@ from api.v1.views import app_views, states
 from flask import Flask, jsonify, Blueprint, make_response
 from models import storage
 import os
+from flask_cors import CORS, cross_origin
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
